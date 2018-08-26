7ba62d2aaec87863ff8227b477347eda49e25bcb
Martynas <mm1544@york.ac.uk>
2018-08-26 21:25:23 +0100

    final

--- starter/chalanges.js
+++ starter/chalanges.js
@@ -12,16 +12,29 @@
         if(gamePlaying) {
 
         //1. Rand. nr.
-        var dice = Math.floor(Math.random()*6)+1;
+        var dice1 = Math.floor(Math.random()*6)+1;
+        var dice2 = Math.floor(Math.random()*6)+1;
 
         //2. Display the result
-        var diceDOM = document.querySelector('.dice'); // in this 
-        diceDOM.style.display = 'block'; 
+        document.getElementById('dice-1').style.display = 'block';
+        document.getElementById('dice-2').style.display = 'block';
  
-        diceDOM.src = 'dice-' + dice + '.png';
+        document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+        document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
 
 
         //3. Update the round s#ore if the rolled nr. was NOT 1
+            if(dice1 !== 1 && dice2 !==1){ 
+            roundScore += dice1 + dice2;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+         
+        } else {
+        
+            nextPlayer();
+
+        }
+            
+            /*
         if(dice === 6 && lastDice === 6) {
             // Player looses s#ore
             scores[activePlayer] = 0;
@@ -40,6 +53,8 @@
         }
             
         lastDice = dice;
+            
+            */
             
       }
     });
@@ -63,12 +78,13 @@
     document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
         
     
+     // reads user input
     var input = document.querySelector('.final-score').value;
     
     var winningScore;
         
     
-    // Undefined, 0 null or "" are COERCED to false
+    // Undefined, 0, null or "" are COERCED to false
     // Anything else is COERCED to true
         if(input) { // if there is an imput
             winningScore = input;
@@ -81,8 +97,8 @@
          document.getElementById('name-' + activePlayer).textContent = 'WINNER!';
          
          // a##essing #ss
-         document.querySelector('.dice').style.display = 'none';
-         
+        document.getElementById('dice-1').style.display = 'none';
+        document.getElementById('dice-2').style.display = 'none';
          // we will a##ess #ss and apply 'winner' #lass to the player that has won
          document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner'); // adds winner #lass
          
@@ -106,7 +122,8 @@
 function nextPlayer() {
     previousSix = false; // indicates that '6' has not occured
     
-    document.querySelector('.dice').style.display = 'none'; // after hitting 'hold' di#e should be hidden 
+    document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none'; // after hitting 'hold' di#e should be hidden 
 
     //!!! toggle 111111111111111
     // 'toggle' removes 'a#tive' #lass if it is pressent and adds if it is missing
@@ -137,7 +154,8 @@ function init() {
 
     gamePlaying = true;
 
-    document.querySelector('.dice').style.display = 'none';  
+    document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none';  
     
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
